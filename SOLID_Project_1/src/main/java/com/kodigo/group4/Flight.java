38cfebb9cc723ae5cd25c837d02bb5857df61627
Hector Montano <74574809+NamelessHGhoul95@users.noreply.github.com>
2021-08-18 07:50:13 -0600

    Added flights attributes
    I just added flights attributes into the entity

--- SOLID_Project_1/src/main/java/com/kodigo/group4/Flight.java
+++ SOLID_Project_1/src/main/java/com/kodigo/group4/Flight.java
@@ -1,5 +1,22 @@
 package com.kodigo.group4;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter @Setter
 public class Flight {
+    private String flightNumber;
+    private String status;
+    private String originCountry;
+    private String originCity;
+    private Date originDate;
+    private Date originTime;
+    private String departureCountry;
+    private String departureCity;
+    private Date arrivalDate;
+    private Date arrivalTime;
+    private boolean landed;
 
 }
